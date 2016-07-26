cfd99a75516fb250a6133b6e431df214ef1b45ff
Taavi Sangel <taavi@stagnationlab.com>
2016-07-26 15:57:24 +0300

    finished footer visuals

--- app/src/components/footer/Footer.js
+++ app/src/components/footer/Footer.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import './footer.scss';
 
 const Footer = () => (
 	<div className="footer">
-		Footer text
+		<a className="row" href="mailto:eliina@vaekas.ee">
+			<span className="icon icon-mail" />
+			eliina@vaekas.ee
+		</a>
+		<a className="row" href="tel:+37255633544">
+			<span className="icon icon-phone" />
+			5563 3544
+		</a>
+		<a className="row" href="www.facebook.com/v2ekas" target="blank">
+			<span className="icon icon-fb" />
+			facebook.com/v2ekas
+		</a>
 	</div>
 );
 
